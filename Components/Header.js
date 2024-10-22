48a2e502f4346afe6211e86084edd2e4d7ebcfca
annanfu <fu.anna@northeastern.edu>
2024-10-22 15:24:22 -0700

    Updated the orientation and useWindowDimensions

--- Components/Header.js
+++ Components/Header.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
 import React from 'react'
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 // update the Header component to accept a prop
 export default function Header({name}) {
+  const { width, height } = useWindowDimensions();
+  console.log(width);
   console.log(name)
   return (
     <View>
+      <Text style={[styles.text, { paddingVertical: height < 415 ? 0 : 10 }]}></Text>
       <Text style={styles.text}>Welcome to {name}!</Text>
     </View>
   )
@@ -13,11 +19,11 @@ export default function Header({name}) {
 
 const styles = StyleSheet.create({
   text: {
-    color: 'darkblue',
-    fontSize: 24,
-    borderColor: 'darkblue',
+    color: "darkblue",
+    fontSize: windowWidth < 380 ? 20 : 26,
+    borderColor: "darkblue",
     borderWidth: 2,
     padding: 5,
     marginBottom: 10,
-  }
-})
+  },
+});

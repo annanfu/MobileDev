2ab9cfe89fe562b31f4462b5dabc31ff4592f713
annanfu <fu.anna@northeastern.edu>
2024-10-22 15:32:18 -0700

    Updated the Header to modify the title

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
+import { StyleSheet, Text, View, useWindowDimensions, Dimensions } from 'react-native'
 import React from 'react'
 
 const windowWidth = Dimensions.get("window").width;
@@ -8,11 +8,11 @@ const windowHeight = Dimensions.get("window").height;
 export default function Header({name}) {
   const { width, height } = useWindowDimensions();
   console.log(width);
-  console.log(name)
   return (
     <View>
-      <Text style={[styles.text, { paddingVertical: height < 415 ? 0 : 10 }]}></Text>
-      <Text style={styles.text}>Welcome to {name}!</Text>
+      <Text style={[styles.text, { paddingVertical: height < 415 ? 0 : 10 }]}>
+      Welcome to {name}!
+      </Text>
     </View>
   )
 }
